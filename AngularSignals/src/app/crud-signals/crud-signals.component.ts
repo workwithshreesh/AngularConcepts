8b398e1a09ddc0108ca92643a8995c8004ef0906
workwithshreesh <shreesht366@gmail.com>
2025-05-12 15:50:40 +0530

    added a project

--- AngularSignals/src/app/crud-signals/crud-signals.component.ts
+++ AngularSignals/src/app/crud-signals/crud-signals.component.ts
@@ -1,29 +1,112 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit, signal, ViewChild } from '@angular/core';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-crud-signals',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './crud-signals.component.html',
   styleUrl: './crud-signals.component.css'
 })
-export class CrudSignalsComponent implements OnInit {
+export class CrudSignalsComponent implements OnInit, AfterViewInit {
 
-  product = signal([{ name:"Product 1", price:100 }, {  name:"Product 2", price:99 }]);
+  // template refrences
+  @ViewChild('addNewProduct') addnewProduct!: ElementRef;
+  private modalInstance!: Modal;
+
+  // forms variable
+  Product!: FormGroup
+
+  // flags
+  Edit:boolean = false;
+  private currentEditName: string | null = null;
+
+
+  product = signal([{ name: "Product 1", price: 100 }, { name: "Product 2", price: 99 }]);
+
+  // dependencie injection
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
-      
+
+    // forms controller
+    this.formControllers();
+
   }
 
-  addProduct(newProduct: {name:string, price:number}){
-    this.product.update((oldProduct)=> [...oldProduct, newProduct])
+  ngAfterViewInit(): void {
+    this.modalInstance = new Modal(this.addnewProduct.nativeElement);
   }
 
-  updateProduct(productName:any){
-    
+  openModal(context: 'Add' | { edit: 'Edit'; data: any }) {
+    if (context === 'Add') {
+      this.Edit = false;
+      this.Product.reset();
+      this.modalInstance.show();
+    } else if (context.edit === 'Edit') {
+      this.Edit = true;
+      this.currentEditName = context.data.name;
+      this.Product.patchValue({
+        productName: context.data.name,
+        productPrice: context.data.price,
+      });
+      this.modalInstance.show();
+    }
   }
 
-  deleteProduct(productName:any){
-    this.product.update((oldProduct) => oldProduct.filter(old => old.name !== productName));
+
+  closeModal() {
+    this.modalInstance.hide()
   }
+
+
+  formControllers() {
+    this.Product = this.fb.group({
+      productName: ['', Validators.required],
+      productPrice: ['', Validators.required]
+    })
+  }
+
+
+  addProduct() {
+    const { productName, productPrice } = this.Product.value;
+    this.product.update(oldProduct => [
+      ...oldProduct,
+      { name: productName, price: +productPrice }
+    ]);
+    this.modalInstance.hide();
+  }
+
+
+  saveEditedProduct() {
+    if (this.Product.invalid || !this.currentEditName) return;
+
+    const { productName, productPrice } = this.Product.value;
+
+    this.product.update((prev) =>
+      prev.map((p) =>
+        p.name === this.currentEditName
+          ? { name: productName, price: +productPrice }
+          : p
+      )
+    );
+
+    this.Product.reset();
+    this.Edit = false;
+    this.currentEditName = null;
+    this.modalInstance.hide();
+  }
+
+
+
+  deleteProduct(productName: string) {
+    this.product.update((prev) =>
+      prev.filter((p) => p.name !== productName)
+    );
+  }
+
 
 }
